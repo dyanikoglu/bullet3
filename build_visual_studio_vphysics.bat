054714672c9edebbeea92b02ced70abaebec2013
Doğa Can Yanıkoğlu <doga.yanikoglu@esensi.com.tr>
2020-01-09 12:11:20 +0300

    Clean premake stuff & unused build batch files

--- build_visual_studio_vphysics.bat
+++
@@ -1,7 +0,0 @@
-IF NOT EXIST bin mkdir bin
-
-cd build3
-
-premake5 --tbb_support --no-enet --no-clsocket --enable_multithreading --no-demos --no-extras --no-bullet3 --targetdir="../bin" vs2013
-
-cd ..
